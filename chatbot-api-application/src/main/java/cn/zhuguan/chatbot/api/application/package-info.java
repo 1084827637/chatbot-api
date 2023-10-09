f4b57efc6d80511aa2417db66991e1a0d31ac07c
zhuguan <gexiguan@bupt.edu.cn>
2023-10-09 11:03:19 +0800

    init ddd project

---
+++ chatbot-api-application/src/main/java/cn/zhuguan/chatbot/api/application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zhuguan
+ * @description
+ **/
+package cn.zhuguan.chatbot.api.application;
---
+++ chatbot-api-interfaces/src/test/java/cn/zhuguan/chatbot/api/test/ApiTest.java
@@ -0,0 +1,8 @@
+package cn.zhuguan.chatbot.api.test;
+
+/**
+ * @author zhuguan
+ * @description 单元测试
+ **/
+public class ApiTest {
+}
